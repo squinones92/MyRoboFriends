b6a12b60c57633e54e6fc39775c45425684bed8d
Marie Quinones <66145422+squinones92@users.noreply.github.com>
2021-09-28 19:09:41 -0400

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots =[
+{
+	id: 1,
+	name: 'Wendoly Quinones',
+	username: 'Gury',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Kiara Arroyo',
+	username: 'Keeyz',
+	email: 'Shana@melissa.tv',
+},
+{
+	id: 3,
+	name: 'Jae Swagg',
+	username: 'SexiChocolate',
+	email: 'Nathan@yesenia.net'
+},
+{
+	id: 4,
+	name: 'Lucy M',
+	username: 'Madre',
+	email: 'Juliane@kory.org'
+},
+{
+	id: 5,
+	name: 'Zen',
+	username: 'Zeni',
+	email: 'zz@buns.com'
+},
+{
+	id: 6,
+	name: 'Jasmin',
+	username: 'Jazy',
+	email: 'kamren@benz.com'
+},
+{
+	id: 7,
+	name: 'Liami',
+	username: 'Salami',
+	email: 'momz@two.net'
+},
+{
+	id: 8,
+	name: 'Omar',
+	username: 'Pops',
+	email: 'dads@father.com'
+},
+{
+	id: 9,
+	name: 'Grams',
+	username: 'Gmaz',
+	email: 'no@idea.org'
+},
+{
+	id: 10,
+	name: 'Cheetahz',
+	username: 'Rawr',
+	email: 'cheetahs@sisters.com'
+}
+]
